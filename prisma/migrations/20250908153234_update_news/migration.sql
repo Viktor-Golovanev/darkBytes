c532271e547716bed06b0bf95ca87b65b1c4df5a
Viktor_Golovanev <holovanev@yandex.by>
2025-09-08 18:33:06 +0300

    update prisma

---
+++ prisma/migrations/20250908153234_update_news/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "public"."news" ADD COLUMN     "imagecontent" TEXT[],
+ADD COLUMN     "tcontent" TEXT;
---
+++ prisma/migrations/20250908153251_update_news2/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."news" ALTER COLUMN "image" DROP NOT NULL;
